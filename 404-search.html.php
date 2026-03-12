2fffdd3372d7452789572d466bb9f1dcbcfc4d13
Emidio Reggiani <emidio.reggiani@cp-spa.it>
2026-03-12 07:56:20 +0100

    Updated search page translations.

--- 404-search.html.php
+++ 404-search.html.php
@@ -12,7 +12,9 @@ if (file_exists($functions_file)) {
 		<h1 class="post-title entry-title"><?php echo i18n('search_results_not_found');?></h1>
 	</header><!-- .entry-header -->
 	<div class="entry-content entry-excerpt">
-	<p>Please search again, or would you like to try our <a href="<?php echo site_url() ?>">homepage</a> instead?</p>
-	<?php echo search() ?>		
+	<p><?php echo i18n('search_again'); ?> <a href="<?php echo site_url() ?>"><?php echo i18n('homepage'); ?></a>?</p>
+	<?php echo search(); ?>	
+	<p>&nbsp;</p>
+	<p>&nbsp;</p>
 	</div><!-- .entry-content -->
 </article>
